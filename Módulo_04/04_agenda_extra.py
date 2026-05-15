dc685b724bf8efd86fc35a67fc12c467c0ae90b4
stefani <stestes181@gmail.com>
2026-05-15 16:20:16 -0300

    Update

--- Módulo_04/04_agenda_extra.py
+++ Módulo_04/04_agenda_extra.py
@@ -2,9 +2,7 @@ agenda = {}
 
 nome = input("Digite o nome: ")
 telefone = input("Digite o telefone: ")
-
 agenda[nome] = telefone
-
 print("\nAGENDA DE CONTATOS")
 
 for nome, telefone in agenda.items():
